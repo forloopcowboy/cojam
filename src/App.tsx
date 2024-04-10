c83aa79f9be232a6f52be228f793b56e6d47d931
L. Gonsalves <lgo@n-side.com>
2024-04-10 18:27:45 +0200

    Basic hello world with tone.js 🎉

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import * as Tone from 'tone';
+import {useRef} from "react";
+
 function App() {
+  const synth = useRef(new Tone.Synth().toDestination()).current;
+
   return (
     <>
       <p>Hello world!</p>
+      <button onClick={() => {
+        const now = Tone.now();
+        const increment = 0.25;
+
+        synth.triggerAttackRelease('C4', '16n');
+        synth.triggerAttackRelease('E4', '16n', now + increment * 1);
+        synth.triggerAttackRelease('G4', '16n', now + increment * 2);
+        synth.triggerAttackRelease('C5', '16n', now + increment * 3);
+        synth.triggerAttackRelease('G4', '16n', now + increment * 4.5);
+        synth.triggerAttackRelease('E4', '16n', now + increment * 5.5);
+        synth.triggerAttackRelease('C4', '16n', now + increment * 6);
+      }}>Play C4</button>
     </>
   )
 }
